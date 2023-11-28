09b841559f374e45ed9353e8c3d021c5f57a0193
JeremiahID <jeremiahefe.je@gmail.com>
2023-11-28 18:14:24 +0100

    Media Link Hover State layout 70% completed

---
+++ index.js
@@ -0,0 +1,33 @@
+// const section = document.querySelector('section');
+// const element = document.querySelector('element');
+
+// element.addEventListener('mouseover', () => {
+//   section.style.display = 'block';
+// });
+
+const Media = document.querySelector('#media');
+const LinkMedia = document.querySelector('#linkMedia');
+const SectionOne = document.querySelector('.sectionOne');
+
+LinkMedia.addEventListener('mouseover', () =>{
+    if (Media.style.display === "none") {
+        SectionOne.classList.toggle('index');
+        Media.style.display = "block";
+
+     } else {
+
+        Media.style.display = "none";
+        
+    
+    }
+    
+});
+
+
+
+// if (Media.style.display === "none") {
+//     Media.style.display = "block";
+//   } else {
+//     Media.style.display = "none";
+    
+//   }
