418f87cc17c8ade9954b367df5dd74ddf0c1635f
JeremiahID <jeremiahefe.je@gmail.com>
2023-11-29 11:16:49 +0100

    Media Hover state 80% done

--- index.js
+++ index.js
@@ -8,19 +8,39 @@
 const Media = document.querySelector('#media');
 const LinkMedia = document.querySelector('#linkMedia');
 const SectionOne = document.querySelector('.sectionOne');
+const LinkEshop = document.querySelector('#linkEshop');
+const Eshop = document.querySelector('#eShop');
 
 LinkMedia.addEventListener('mouseover', () =>{
     if (Media.style.display === "none") {
-        SectionOne.classList.toggle('index');
         Media.style.display = "block";
+        Media.style.display = "flex";
+        Media.style.transition = 'all 1s ease-in-out';
 
      } else {
 
         Media.style.display = "none";
         
     }
+
     
 });
+
+
+LinkEshop.addEventListener('mouseover', () =>{
+    if (Eshop.style.display === "none") {
+        Eshop.style.display = "block";
+
+     } else {
+
+        Eshop.style.display = "none";
+        
+    }
+    
+    
+});
+
+
 
 
 
