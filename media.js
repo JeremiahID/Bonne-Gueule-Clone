f58a0a5f383842609ce22592ac454d7374828a02
JeremiahID <jeremiahefe.je@gmail.com>
2024-01-02 11:57:37 +0100

    mdeia completed

--- media.js
+++ media.js
@@ -4,11 +4,11 @@ const MediaSectionOne = document.querySelector('.Media-section-one');
 const MediaSectionTwo = document.querySelector('.Media-section-two');
 const MediaSectionThree = document.querySelector('.Media-section-three');
 const MediaSectionFour = document.querySelector('.Media-section-four');
-const MediaSectioFive = document.querySelector('.Media-section-five');
-const MediaSectioSix = document.querySelector('.Media-section-six');
-const MediaSectioSeven = document.querySelector('.Media-section-seven');
-const MediaSectioEight = document.querySelector('.Media-section-eight');
-const MediaSectioNine = document.querySelector('.Media-section-nine');
+const MediaSectionFive = document.querySelector('.Media-section-five');
+const MediaSectionSix = document.querySelector('.Media-section-six');
+const MediaSectionSeven = document.querySelector('.Media-section-seven');
+const MediaSectionEight = document.querySelector('.Media-section-eight');
+const MediaSectionNine = document.querySelector('.Media-section-nine');
 const mediaSectionFour = document.querySelector('.sectionFour');
 const LinksMedia = document.querySelector('#anchor');
 
@@ -21,11 +21,11 @@ Hamburg.addEventListener('click' , () =>{
         MediaSectionTwo.classList.add('hide');
         MediaSectionThree.classList.add('hide');
         MediaSectionFour.classList.add('hide');
-        MediaSectioFive.classList.add('hide');
-        MediaSectioSix.classList.add('hide');
-        MediaSectioSeven.classList.add('hide');
-        MediaSectioEight.classList.add('hide');
-        MediaSectioNine.classList.add('hide');
+        MediaSectionFive.classList.add('hide');
+        MediaSectionSix.classList.add('hide');
+        MediaSectionSeven.classList.add('hide');
+        MediaSectionEight.classList.add('hide');
+        MediaSectionNine.classList.add('hide');
         mediaSectionFour.classList.add('hide');
     } else {
         LinksMedia.style.display = 'none';
@@ -33,11 +33,11 @@ Hamburg.addEventListener('click' , () =>{
         MediaSectionTwo.classList.remove('hide');
         MediaSectionThree.classList.remove('hide');
         MediaSectionFour.classList.remove('hide');
-        MediaSectioFive.classList.remove('hide');
-        MediaSectioSix.classList.remove('hide');
-        MediaSectioSeven.classList.remove('hide');
-        MediaSectioEight.classList.remove('hide');
-        MediaSectioNine.classList.remove('hide');
+        MediaSectionFive.classList.remove('hide');
+        MediaSectionSix.classList.remove('hide');
+        MediaSectionSeven.classList.remove('hide');
+        MediaSectionEight.classList.remove('hide');
+        MediaSectionNine.classList.remove('hide');
         mediaSectionFour.classList.remove('hide');
     }
 })
