87842afbb12f73aa5ce3a30a89914d648f0b5222
JeremiahID <jeremiahefe.je@gmail.com>
2024-01-17 20:45:26 +0100

    test

--- shop.js
+++ shop.js
@@ -1,8 +1,8 @@
 const ham_burgs = document.querySelector('#hamburger');
 const Agenda_section_one_shop = document.querySelector('')
 const shop_section_one_main= document.querySelector('.shop-section-one-main');
-const shop_section_one = document.querySelector('');
-const shop_section_two = document.querySelector('');
+const shop_section_one = document.querySelector('.shop-section-one');
+const shop_section_two = document.querySelector('.shop-section-one');
 const shop_section_three = document.querySelector('');
 const shop_section_four = document.querySelector('');
 const shop_section_five = document.querySelector('');
