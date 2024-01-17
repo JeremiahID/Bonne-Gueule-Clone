234a5f7b3f6154116a8cde971176074d0a894444
JeremiahID <jeremiahefe.je@gmail.com>
2024-01-17 20:47:00 +0100

    shop java script updates

--- shop.js
+++ shop.js
@@ -1,11 +1,11 @@
 const ham_burgs = document.querySelector('#hamburger');
-const Agenda_section_one_shop = document.querySelector('')
+const Agenda_section_one_shop = document.querySelector('.Agenda-section-one')
 const shop_section_one_main= document.querySelector('.shop-section-one-main');
 const shop_section_one = document.querySelector('.shop-section-one');
-const shop_section_two = document.querySelector('.shop-section-one');
-const shop_section_three = document.querySelector('');
-const shop_section_four = document.querySelector('');
-const shop_section_five = document.querySelector('');
+const shop_section_two = document.querySelector('.shop-section-two');
+const shop_section_three = document.querySelector('.shop-section-three');
+const shop_section_four = document.querySelector('.shop-section-four');
+const shop_section_five = document.querySelector('.shop-section-five');
 
 
 ham_burgs.addEventListener('click' , ()=>{
